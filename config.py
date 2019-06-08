ecc79839bdb64ab8cf8bb2b6b753a22077b45125
Preston <pmar116@uw.edu>
2019-06-07 21:31:29 -0700

    try x6

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-	LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-secret-key'
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
--- nmp_app.py
+++ nmp_app.py
@@ -1,24 +1,27 @@
 from flask import Flask
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-if app.config['LOG_TO_STDOUT']:
-    stream_handler = logging.StreamHandler()
-	stream_handler.setLevel(logging.INFO)
-	app.logger.addHandler(stream_handler)
-else:
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-	file_handler.setFormatter(logging.Formatter(
-		'%(asctime)s %(levelname)s: %(message)s '
-		'[in %(pathname)s:%(lineno)d]'))
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
-app.logger.setLevel(logging.INFO)
-app.logger.info('nemomappy startup')
-
+if not app.debug and not app.testing:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 from app import routes
 
