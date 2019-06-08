1080c6abd4ce006808c6f95edfd04fa76221795e
Preston <pmar116@uw.edu>
2019-06-07 21:17:43 -0700

    try again x4

--- nmp_app.py
+++ nmp_app.py
@@ -4,10 +4,5 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
-if app.config['LOG_TO_STDOUT']:
-    stream_handler = logging.StreamHandler()
-	stream_handler.setLevel(logging.INFO)
-	app.logger.addHandler(stream_handler)
-
 from app import routes
 
