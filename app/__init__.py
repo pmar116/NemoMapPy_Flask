2c338cada8717b2480062c7e905d97366ceff4e2
Preston <pmar116@uw.edu>
2019-06-07 20:43:11 -0700

    try this instead

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,27 @@
+from flask import Flask
+from config import Config
 
+app = Flask(__name__)
+app.config.from_object(Config)
+
+if app.config['LOG_TO_STDOUT']:
+    stream_handler = logging.StreamHandler()
+	stream_handler.setLevel(logging.INFO)
+	app.logger.addHandler(stream_handler)
+else:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter(
+		'%(asctime)s %(levelname)s: %(message)s '
+		'[in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	
+app.logger.setLevel(logging.INFO)
+app.logger.info('nemomappy startup')
+
+from app import routes
+
+if __name__ == '__main__':
+        app.run()
